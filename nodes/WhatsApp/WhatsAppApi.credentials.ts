3dde30fb64277855ab3f63d6f201f50ab2c8a66c
carlos2325 <carlos2325@users.noreply.github.com>
2025-05-03 21:38:09 +0200

    Update WhatsAppApi.credentials.ts

--- nodes/WhatsApp/WhatsAppApi.credentials.ts
+++ nodes/WhatsApp/WhatsAppApi.credentials.ts
@@ -1,1 +1,33 @@
+import {
+  ICredentialType,
+  INodeProperties,
+} from 'n8n-workflow';
 
+export class WhatsAppApi implements ICredentialType {
+  name = 'whatsAppApi';
+  displayName = 'WhatsApp API';
+  documentationUrl = '';
+  properties: INodeProperties[] = [
+    {
+      displayName: 'API Key',
+      name: 'apiKey',
+      type: 'string',
+      default: '',
+    },
+    {
+      displayName: 'API Secret',
+      name: 'apiSecret',
+      type: 'string',
+      typeOptions: {
+        password: true,
+      },
+      default: '',
+    },
+    {
+      displayName: 'Phone Number ID',
+      name: 'phoneNumberId',
+      type: 'string',
+      default: '',
+    },
+  ];
+}
