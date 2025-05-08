d0488ea8384331322bcb91afe0a41fd9e9c6964d
carlos2325 <carlos2325@users.noreply.github.com>
2025-05-08 07:06:39 +0200

    Update gulpfile.js
    Agregar configuración de Gulp para iconos"

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 
+const { src, dest } = require('gulp');
+
+function buildIcons() {
+  return src('WhatsApp/*.svg')
+    .pipe(dest('dist/WhatsApp/'));
+}
+
+exports.default = buildIcons;
+exports['build:icons'] = buildIcons;
