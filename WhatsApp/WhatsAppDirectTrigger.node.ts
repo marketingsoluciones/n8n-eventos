45fc19d4500e59bdbe6efc69e3b69d052a7685d2
carlos2325 <carlos2325@users.noreply.github.com>
2025-05-12 06:53:11 +0200

    Update WhatsAppDirectTrigger.node.ts

--- WhatsApp/WhatsAppDirectTrigger.node.ts
+++ WhatsApp/WhatsAppDirectTrigger.node.ts
@@ -118,7 +118,7 @@ export class WhatsAppDirectTrigger implements INodeType {
       
       
       // Verificación de firma HMAC para mayor seguridad
-      const signature = req.headers['x-hub-signature-256
+      const signature = req.headers['x-hub-signature-256'];
 
 
   // Insertar aquí
