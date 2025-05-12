0a209166d333ed0b9bb971a7ae2b05d34c9835dc
carlos2325 <carlos2325@users.noreply.github.com>
2025-05-12 19:45:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
   // Exportar los nodos
   WhatsAppDirectNode: require('./dist/WhatsApp/WhatsAppDirectNode.node').WhatsAppDirectNode,
   WhatsAppDirectTrigger: require('./dist/WhatsApp/WhatsAppDirectTrigger.node').WhatsAppDirectTrigger,
-  
+   // Nuevo nodo
+  WhatsAppNativeWebhook: require('./dist/WhatsApp/WhatsAppNativeWebhook.node').WhatsAppNativeWebhook,
   // Exportar las credenciales
   WhatsAppApi: require('./dist/credentials/WhatsAppApi.credentials').WhatsAppApi,
 };
